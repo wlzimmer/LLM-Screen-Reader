d55c4aba860f9462bbfbcf16355cc46da7165cf3
wlzimmer <5460684+wlzimmer@users.noreply.github.com>
2025-08-27 11:41:36 -0400

    Type into chatGPT and speak the result

--- background.js
+++ background.js
@@ -1,14 +1,21 @@
 var rate = 1.5
 var volume = 1.0
+var line = ''
+
 chrome.runtime.onMessage.addListener(
   function (msg, sender, sendResponse) {
 	chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 		chrome.tabs.sendMessage(tabs[0].id, msg);
-		chrome.tts.speak(msg, {'enqueue': true, 'lang': 'en-US', 'rate': rate, 'volume': volume})
-		console.log ('fubar')
-		var API_KEY = "sk-***************"
-			console.log (`Bearer ${API_KEY}`)
-			fetch("https://api.openai.com/v1/chat/completions", {
+			
+		if (msg != 'Enter') {
+//			speak (msg)
+			if (msg == 'Backspace') {
+				line = line.slice(-1)
+			} else {
+				line += msg
+			}
+		} else {
+			var API_KEY = "sk-*****", {
 			  method: "POST",
 			  headers: {
 				"Content-Type": "application/json",
@@ -16,10 +23,14 @@ chrome.runtime.onMessage.addListener(
 			  },
 			  body: JSON.stringify({
 				model: "gpt-4.1-nano",
-				messages: [{ role: "user", content: "how many eggs in a dozen" }]
+				messages: [{ role: "user", content: line }]
 			  })
-			}).then(response => response.json()).then(response => console.log(JSON.stringify(response)));
+			}).then(response => response.json()).then(response => speak(JSON.stringify(response.choices[0].message.content)));
+		}
 	});
 	return true;
 })
 
+function speak (msg) {
+	chrome.tts.speak(msg, {'enqueue': true, 'lang': 'en-US', 'rate': rate, 'volume': volume})
+}
